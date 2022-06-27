5d830d94691074d39681ff394f0926a5ce265e77
Snowball558 <doved558@gmail.com>
2022-06-27 14:34:27 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let listDigitsArray = [...(String(n))]
+    if (listDigitsArray[0] === '-') {
+        listDigitsArray.shift(0)
+    }
+    const reverseNumber = Number(listDigitsArray.reverse().join(''))
+    return reverseNumber
 }
